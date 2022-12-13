5cc720b66b0bb26cc1050471c0c8bde1ad1db6a7
ibramila <70vq7w9@code.edu.az>
2022-12-13 01:24:26 -0800

    created add butn

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,9 @@ const surname = document.querySelector(".surname");
 const title = document.querySelector(".title");
 const story = document.querySelector(".story");
 const gender = document.querySelector(".gender");
+const infoBtn = document.querySelector(".infoBtn");
+
+
 
 addBtn.addEventListener("click", modalFunc);
 closeModal.addEventListener("click", closeModalFunc);
@@ -24,23 +27,29 @@ function saveFunc() {
     modal.style.display = "none"
 
 }
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 fetch('http://localhost:3000/users')
     .then((response) => response.json())
     .then((data) =>
         data.forEach(user => {
-            let card = `<div class="col-3">
+            cards.innerHTML += `<div class="col-3">
                     <div class="card border-success mb-3" style="max-width: 18rem;">
                         <div class="card-header bg-transparent border-success">${user.name}</div>
                         <div class="card-body text-success">
-                            <h5 class="card-title">${user.surname}</h5>
+                            <h5 class="card-title">${user.age}</h5>
                             <span class="card-text">${user.gender}</span>
                         </div>
                         <div class="card-footer bg-transparent border-success">
                         <button type="button" class="btn btn-white"><i class="fa-solid fa-trash"></i>
                         </button>
-                        <button type="button" class="btn btn-white"><i class="fa-solid fa-circle-info"></i>
+                        <button type="button" class="btn btn-white infoBtn"><i class="fa-solid fa-circle-info"></i>
                         </button>
                         </div>
                     </div>
                 </div>`
+
         }));
